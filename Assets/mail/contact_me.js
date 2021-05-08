b637ffc0421a948daa4ebda7cf64e7f9e82eb8c7
vortex73 <32898329+vortex73@users.noreply.github.com>
2021-05-08 23:36:43 +0530

    Add files via upload

---
+++ Assets/mail/contact_me.js
@@ -0,0 +1,107 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+$(function () {
+    $(
+        "#contactForm input,#contactForm textarea,#contactForm button"
+    ).jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function ($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name1 = $("input#name").val();
+            var email1 = $("input#email").val();
+            var phone1 = $("input#phone").val();
+            var message1 = $("textarea#message").val();
+            var firstName = name1; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(" ") >= 0) {
+                firstName = name1.split(" ").slice(0, -1).join(" ");
+            }
+            $this = $("#sendMessageButton");
+            $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+            const csrftoken = getCookie('csrftoken');
+            $.ajax({
+                url: '/homemail',
+                type: "post",
+                data: {
+                    name : name1,
+                    phone : phone1,
+                    email : email1,                                                                                                                                                                                                           
+                    message : message1,                                                                                                                                                                                                                                                                                                                                                                                               
+                },
+                headers: {'X-CSRFtoken': csrftoken},
+                cache: false,
+                success: function () {
+                    // Success message
+                    $("#success").html("<div class='alert alert-success'>");
+                    $("#success > .alert-success")
+                        .html(
+                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                        )
+                        .append("</button>");
+                    $("#success > .alert-success").append(
+                        "<strong>Your message has been sent. </strong>"
+                    );
+                    $("#success > .alert-success").append("</div>");
+                    //clear all fields
+                    $("#contactForm").trigger("reset");
+                },
+                error: function () {
+                    // Fail message
+                    $("#success").html("<div class='alert alert-danger'>");
+                    $("#success > .alert-danger")
+                        .html(
+                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                        )
+                        .append("</button>");
+                    $("#success > .alert-danger").append(
+                        $("<strong>").text(
+                            "Sorry " +
+                                firstName +
+                                ", it seems that our mail server is not responding. Please try again later!"
+                        )
+                    );
+                    $("#success > .alert-danger").append("</div>");
+                    //clear all fields
+                    $("#contactForm").trigger("reset");
+                },
+                complete: function () {
+                    setTimeout(function () {
+                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+                    }, 1000);
+                },
+            });
+        },
+        filter: function () {
+            return $(this).is(":visible");
+        },
+    });
+
+   
+    $('a[data-toggle="tab"]').click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$("#name").focus(function () {
+    $("#success").html("");
+});
